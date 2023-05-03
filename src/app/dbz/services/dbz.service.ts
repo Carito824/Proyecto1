fc9ad1f88ba225b1ec65bd388fec15c2e2a037cf
Carolina Leon <sclh2408@gmail.com>
2023-05-02 21:02:05 -0500

    ... OK

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DbzService{
     public characters: Character[] = [{
-        // id: uuidv4(),
+        id: uuidv4(),
         name: "Krillin",
         power: 1000,
         
     }, {
-        // id: uuidv4(), 
+        id: uuidv4(), 
        name: "Goku",
        power: 10000
     },
     {
-    //    id: uuidv4(), 
+       id: uuidv4(), 
        name: "Vegeta",
        power: 7500
     }
 ];
     addCharacter(character: Character): void{
-    // const newCharacter: Character = { id: uuidv4(), ...character };
+    const newCharacter: Character = { id: uuidv4(), ...character };
         this.characters.push(character);
 }
     /*onDeleteCharacter(index:number) {
